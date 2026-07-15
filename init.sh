b8da3fccbb8fe49a868b063bd290f1093af290f0
CodingManoj <mvp2612@hotmail.com>
2026-07-14 22:24:35 -0400

    Helm: Payment values

--- init.sh
+++ init.sh
@@ -6,6 +6,18 @@ action=$3
 
 services=(catalogue user cart shipping frontend )
 
+# if $1 or 2 or 3 or all are empty, exit the script and output the usage information
+if [[ -z "$app" || -z "$env" || -z "$action" ]]; then
+  echo "Error: Missing required arguments."
+  echo "Usage: $0 <app> <env> <action>"
+  exit 1
+fi  
+
+echo "Usage: $0 <app> <env> <action>"
+echo "  <app>: all or specific service name"
+echo "  <env>: environment name"
+echo "  <action>: install or uninstall"
+
 if [[ $app == "all" ]]; then
   case "$action" in
     install)
@@ -20,3 +32,6 @@ if [[ $app == "all" ]]; then
       ;;
   esac
 fi 
+
+
+
