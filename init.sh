3ff2de9a452c3ec211250c165cc6fa7751fc2a83
CodingManoj <mvp2612@hotmail.com>
2026-07-14 22:25:39 -0400

    Helm: Payment values

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ services=(catalogue user cart shipping frontend )
 # if $1 or 2 or 3 or all are empty, exit the script and output the usage information
 if [[ -z "$app" || -z "$env" || -z "$action" ]]; then
   echo "Error: Missing required arguments."
-  echo "Usage: $0 <app> <env> <action>"
+  echo "Usage: $0 <app> <env> <action- install/uninstall>"
   exit 1
 fi  
 
